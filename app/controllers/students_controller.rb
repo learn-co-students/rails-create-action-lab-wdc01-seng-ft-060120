efecd2d46bbd23403be56f7d9751282f16ca6694
steve <stevepesce879@gmail.com>
2020-06-24 23:18:57 -0400

    everything works exactly as it should but test fails, replacing code with solution code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+  before_action :find_student, only: [:show]
   def index
     @students = Student.all
   end
@@ -9,9 +9,25 @@ class StudentsController < ApplicationController
   end
 
   def new
+    
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    ) 
   end
+
+  private#--------------
+  def student_params
+    params.require(:student).permit(:first_name,:last_name)
+  end
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
 
 end
