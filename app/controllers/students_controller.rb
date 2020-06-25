d1d8fe2f3f784b1bc2ccf3c405f82f9dd1b30f0e
steve <stevepesce879@gmail.com>
2020-06-24 23:21:12 -0400

    changed to pass test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,7 +18,8 @@ class StudentsController < ApplicationController
         first_name: params[:first_name],
         last_name: params[:last_name]
       }
-    ) 
+    )
+    redirect_to student_path(@student) 
   end
 
   private#--------------
